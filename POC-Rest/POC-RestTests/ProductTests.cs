ef98655992b2600c8f0eb79f3694e3b98c7b820a
drr00t <adribeiro@gmail.com>
2022-02-23 11:10:07 -0300

    criando projeto de teste e melhorando gitignore

---
+++ POC-Rest/POC-RestTests/ProductTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace POC_RestTests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            using var api = new SUTApi();
+            var client = api.CreateClient();
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, client.GetProducts().GetAwaiter().GetResult().StatusCode);
+        }
+    }
+}
